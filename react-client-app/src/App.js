6b68aa40a46324dd6d6499f7d86a273d8507dfef
Oliver <oliver.kuhn@ancud.de>
2023-06-02 10:03:54 +0200

    add first class component & function component

--- react-client-app/src/App.js
+++ react-client-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Example } from './components/Example';
+import { ClassExample } from './components/ClassExample';
 
 function App() {
   return (
@@ -18,6 +20,13 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Example color="gelb"/>
+      </div>
+
+      <div>
+        <ClassExample color="blau" />
+      </div>
     </div>
   );
 }
---
+++ react-client-app/src/components/ClassExample.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export class ClassExample extends React.Component {
+    render() {
+      return <h1>Hello, test color: { this.props.color }</h1>;
+    }
+}
+
