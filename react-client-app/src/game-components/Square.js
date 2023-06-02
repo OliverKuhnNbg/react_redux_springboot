ae6463fdef7e3c900f02e2c442e3d66deb639786
Oliver <oliver.kuhn@ancud.de>
2023-06-02 14:07:26 +0200

    change square to a function component

--- react-client-app/src/game-components/Square.js
+++ react-client-app/src/game-components/Square.js
@@ -1,12 +1,9 @@
 import React from "react";
 
-export class Square extends React.Component {
-
-    render() {
-        return (
-            <button className="square" onClick={ () => this.props.onClickAction() }>
-                { this.props.value }
-            </button>
-        );
-    }
+export function Square(props) {
+    return (
+        <button className="square" onClick={ () => props.onClickAction() }>
+            { props.value }
+        </button>
+    );
 }
