770d74e012215765355e6ef8b983aa9660b0934b
Oliver <oliver.kuhn@ancud.de>
2023-06-09 11:01:03 +0200

    import bootstrap 5 to app component

--- maisel-rampen-booking-big-calendar/src/App.js
+++ maisel-rampen-booking-big-calendar/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>big calendar app tets</h1>
+      <h1 className='text-center text-uppercase'>big calendar App-Test</h1>
     </div>
   );
 }
