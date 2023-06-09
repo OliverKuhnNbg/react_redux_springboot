4361974f69300b9354b3aeaec710e8931617a2da
Oliver <oliver.kuhn@ancud.de>
2023-06-09 15:39:59 +0200

    cal load on current date

--- maisel-rampen-booking-big-calendar/src/App.js
+++ maisel-rampen-booking-big-calendar/src/App.js
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
-import events from "./events";
+import events from './events';
+import { getCurrentDayDate } from './util'
 
 
 const localizer = momentLocalizer(moment);
@@ -13,18 +14,28 @@ console.log(localizer)
 //const allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k]);
 
 function App() {
+  const currentDate = getCurrentDayDate();
+
   return (
     <div className="App">
-      <h1 className='text-center text-uppercase'>big calendar App-Test</h1>
-      <div className="calendar-wrapper">
-        <BigCalendar
-          localizer={localizer}
-          events={events}
-          step={60}
-          defaultDate={new Date(2015, 3, 1)}
-          popup={false}
-          onShowMore={(events, date) => this.setState({ showModal: true, events })}
-        />
+      <h1 className="text-center text-uppercase">big calendar App-Test</h1>
+      <div className="row ">
+        <div className="col-12 my-4 text-center">
+          <span className="h-5">aktuelles Datum:</span> <span className='fw-bold'>{currentDate.format("DD.MM.YYYY")}</span>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="calendar-wrapper">
+          <BigCalendar
+            localizer={localizer}
+            events={events}
+            step={60}
+            defaultDate={currentDate}
+            popup={false}
+            onShowMore={(events, date) => this.setState({ showModal: true, events })}
+          />
+        </div>
       </div>
     </div>
   );
