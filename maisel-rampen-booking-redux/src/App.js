f2f7f62d5b407c730a2a98d03161004b16768dea
Oliver <oliver.kuhn@ancud.de>
2023-06-14 12:15:32 +0200

    react add bootstrap 5

--- maisel-rampen-booking-redux/src/App.js
+++ maisel-rampen-booking-redux/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import EventBookingForm from './features/eventbookingform/EventBookingForm';
 
 
 function App() {
   return (
-    <div className="App">
-      <EventBookingForm />
+    <div className='App'>
+      <div className='container-fluid'>
+        <EventBookingForm />
+      </div>
+      <div className='row'>
+        <div className='col-12 alert alert-primary' role='alert'>
+          A simple primary alert—check it out!
+        </div>
+      </div>
     </div>
   );
 }
