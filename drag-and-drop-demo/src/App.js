fcf8b5ace60d9ba19c1620f3ea3f5a6556118889
Oliver <oliver.kuhn@ancud.de>
2023-09-26 17:05:28 +0200

    init drag and drop elements

--- drag-and-drop-demo/src/App.js
+++ drag-and-drop-demo/src/App.js
@@ -1,16 +1,64 @@
+import { useState } from 'react';
+import { DATA } from './util';
 import logo from './logo.svg';
 import './App.css';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import DragAndDrop from './components/DragAndDrop';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 
 function App() {
+
+  const [stores, setStores] = useState(DATA);
+  const handleDragDrop = (results) => {
+    console.log('drag-n-Drop function called');
+    console.log(results);
+    const {source, destination, type} = results;
+
+    if(!destination) return;
+    if(source.droppableId === destination.droppableId && source.index === destination.index) return;
+    if(type === 'group') {
+      const reorderedStores = [...stores];
+
+      const sourceIndex = source.index
+      const destinationIndex = destination.index
+      console.log(source.index)
+      console.log(destination.index)
+      const [removedStore] = reorderedStores.splice(sourceIndex, 1);
+      reorderedStores.splice(destinationIndex, 0, removedStore);
+
+      return setStores(reorderedStores);
+    }
+
+
+  };
+
   return (
-    <DndProvider backend={ HTML5Backend }>
-      <div className="App">
-        <DragAndDrop />
+      <div className='layout__wrapper'>
+          <div className='card'>
+            <DragDropContext onDragEnd={handleDragDrop}>
+              <div className='header'>
+                <h1>Shopping List</h1>
+              </div>
+              <Droppable droppableId='ROOT' type="group">
+                {(provided) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {stores.map((store, index) => 
+                      <Draggable draggableId={store.id} key={store.id} index={index}>
+                        {(provided) => (
+                          <div className='store-container' 
+                            {...provided.dragHandleProps} 
+                            {...provided.draggableProps} 
+                            ref={provided.innerRef} >
+                            <h3>{store.name}</h3>
+                          </div>
+                        )}
+                      </Draggable>
+                    )}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </div>
       </div>
-    </DndProvider>
   );
 }
 
--- drag-and-drop-demo/src/components/PicturePanel.js
+++ drag-and-drop-demo/src/components/PicturePanel.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useDrag } from 'react-dnd';
 
 function PicturePanel({pictureId, pictureUrl}) {
-  return (
-    <img src={pictureUrl} /> 
-  )
+    /* const [{isDragging}, drag] = useDrag(() => {
+        type: "image"
+    }); */
+
+    return (
+        <img  src={ pictureUrl } /> 
+    )
 }
 
 export default PicturePanel
